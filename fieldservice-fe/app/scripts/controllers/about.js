f2f0d4f27d25df4152f5b5850d9f5741291cc8bb
Beltsazar <beltsazar.register@gmail.com>
2015-12-06 21:48:51 +0100

    Working yeoman angular app!!!

---
+++ fieldservice-fe/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fieldserviceFeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fieldserviceFeApp
+ */
+angular.module('fieldserviceFeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ fieldservice-fe/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fieldserviceFeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fieldserviceFeApp
+ */
+angular.module('fieldserviceFeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
