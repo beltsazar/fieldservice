a5ec5aea53dfec0d45c3fe30435c83424264a1c2
Beltsazar <beltsazar.register@gmail.com>
2015-12-11 17:59:49 +0100

    Added spec runner to protractor config

--- fieldservice-fe/test/protractor.conf.js
+++ fieldservice-fe/test/protractor.conf.js
@@ -3,5 +3,16 @@ exports.config = {
   framework: 'jasmine',
   //seleniumAddress: 'http://localhost:4444/wd/hub',
   seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar',
-  specs: ['protractor/spec/*.js']
+  specs: ['protractor/spec/*.js'],
+  onPrepare: function() {
+      var SpecReporter = require('jasmine-spec-reporter');
+      // add jasmine spec reporter
+      jasmine.getEnv().addReporter(new SpecReporter({
+    	  displayStacktrace: 'all',
+    	  colors: false
+    	  }));
+   },
+   jasmineNodeOpts: {
+	   print: function() {}
+   }
 }
